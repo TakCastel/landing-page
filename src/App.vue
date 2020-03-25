9db9f3a77f6ab42e56fea92f5d0a20861ac430e7
Tak Castel <takcastel@gmail.com>
2020-03-25 11:20:56 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Brumes Roleplay</h1>
+    <p>Bienvenue sur le site officiel de la Guilde Brumes.</p>
+    <p>Avant de porusuivre, sélectionnez votre jeu.</p>
+    <div class="row">
+      <Card 
+        isDisabled
+        redirectionLink=""
+        :backgroundImage="require('./assets/bg-ffxiv.jpg')"
+        alternativeText="Fond d'écran de Final Fantasy XIV"
+      />
+      <Card
+        redirectionLink=""
+        :backgroundImage="require('./assets/bg-teso.jpg')"
+        alternativeText="Fond d'écran de The Elder Scrolls Online"
+      />
+      <Card
+        isDisabled
+        redirectionLink=""
+        :backgroundImage="require('./assets/bg-wow.jpg')"
+        alternativeText="Fond d'écran de World of Warcraft"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+}
+
+body {
+  min-height: 100vh;
+  background: linear-gradient(-45deg, #212121, #252323);
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 56px;
+  color: #ebe6e4;
+}
+
+.row {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 28px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="game" @click="handleClick" :class="{ disabled: isDisabled }">
+    <img :src="backgroundImage" :alt="alternativeText">
+    <p v-if="isDisabled" class="unavailable">Indisponible</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    redirectionLink: {
+      type: String,
+      default: '',
+      required: true
+    },
+    backgroundImage: {
+      type: String,
+      default: '',
+      required: true
+    },
+    alternativeText: {
+      type: String,
+      default: '',
+      required: true
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+      required: false
+    }
+  },
+
+  methods: {
+    handleClick() {
+      window.location.href = this.redirectionLink
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  transition: filter .3s;
+}
+.game {
+  cursor: pointer;
+  flex: 1;
+  margin: 1rem;
+  position: relative;
+  box-shadow: 0.1em 0.1em .8em rgba(0, 0, 0, 0.9);
+  transition: transform .3s;
+  border-radius: 4px;
+  overflow: hidden;
+  max-width: 300px;
+}
+.game:hover {
+  box-shadow: .1em .1em 1em -0.3em rgba(0, 0, 0);
+  transform: scale(1.05)
+}
+.disabled {
+  cursor: not-allowed;
+}
+.disabled img:hover {
+  filter: opacity(.1);
+}
+
+.unavailable {
+  pointer-events: none;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: calc(50%);
+  margin: auto;
+  -webkit-transform: rotate(-22deg);
+  transform: rotate(-14deg) translate(-18%, 0);
+  background: linear-gradient(45deg, #212020, #00000096);
+  width: 150%;
+  padding: 1em;
+  font-weight: 700;
+  color: #f33f31;
+}
+
+img {
+  height: 100%;
+  width: 100%;
+}
+</style>
